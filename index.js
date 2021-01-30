feb186f876e3e80465ed8fde5cbc78f801896299
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-01-30 21:13:18 +0000

    view employees by department fumction added

--- index.js
+++ index.js
@@ -84,3 +84,12 @@ function viewAllRoles() {
     startPrompt()
     })
   }
+
+  function viewAllDepartments() {
+    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      startPrompt()
+    })
+  }
