28812f2d750c2bebed267b38efacb8520e7cb244
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-01-30 20:26:18 +0000

    tables populated

--- schema.sql
+++ schema.sql
@@ -4,16 +4,26 @@ CREATE database employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE department (
-    id int AUTO_INCREMENT PRIMARY KEY,
-    name varchar(30)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
-  
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
- 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 SELECT * FROM department;
